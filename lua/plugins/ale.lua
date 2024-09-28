8b689943c65e8397072899ed387d8b93ba23ed07
Kevin Pankonen <kevin@redefine.org>
2024-09-27 19:17:48 -0700

    add another keymap for ALEToggle

--- lua/plugins/ale.lua
+++ lua/plugins/ale.lua
@@ -6,6 +6,7 @@ return {
       vim.keymap.set("n", "<C-j>", "<Plug>(ale_next_wrap)", key_opts)
       vim.keymap.set("n", "<C-k>", "<Plug>(ale_previous_wrap)", key_opts)
       vim.keymap.set("n", "<leader>a", "<cmd>ALEToggle<cr>", key_opts)
+      vim.keymap.set("n", "<F3>", "<cmd>ALEToggle<cr>", key_opts)
     end,
   },
 }
